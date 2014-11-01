d27fd675183046baadba6250720b12e3714a1ef3
Paolo Bueno <paolohaji@gmail.com>
2014-11-01 20:15:37 +1100

    p20

---
+++ p20/p20.go
@@ -0,0 +1,20 @@
+/*
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+*/
+package main
+
+import "fmt"
+import "math/big"
+import "github.com/paolobueno/euler/specifics"
+
+func main() {
+	theNumber := big.NewInt(0).MulRange(1, 100)
+	result := specifics.AddDigits(theNumber)
+	fmt.Println(theNumber)
+	fmt.Println(result)
+}
